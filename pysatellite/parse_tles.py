fd80aa738d67ca82a4ea2e2754664a16e78260a6
Benedict Oakes <sgboakes@liverpool.ac.uk>
2023-07-04 16:39:19 +0100

    Further TLE work

--- pysatellite/parse_tles.py
+++ pysatellite/parse_tles.py
@@ -7,13 +7,16 @@ import pandas as pd
 import os
 import re
 from skyfield.api import EarthSatellite, load, wgs84
+from sgp4.api import Satrec
 
-# Load json containing TLEs
-# Maybe need to sanitise first? (Ensure only one space between elements)
+# Unclean file
 file = os.getcwd() + '/space-track_leo_tles.txt'
-
+# Cleaned file
 file_sanitised = os.getcwd() + '/space-track_leo_tles_sanitised.txt'
 
+# Can probably leave this commented once already been cleaned
+# But keep around in case new TLEs need to be sanitised
+# Should leave date on TLE file or run with set time??
 lines = []
 with open(file) as f:
     for line in f.readlines():
@@ -22,16 +25,30 @@ with open(file) as f:
 with open(file_sanitised, 'w') as f:
     f.writelines(lines)
 
-df = pd.read_csv(file_sanitised, sep=' ', header=None, on_bad_lines='skip')
+# df = pd.read_csv(file_sanitised, sep=' ', h
 
-df_line1 = df.iloc[::2, :]
-df_line1.columns = ['Line Number', 'Satellite Catalog Number', 'Epoch', 'd1 Mean Motion', 'd2 Mean Motion', 'B*',
-                    'Ephemeris Type','Element Set No.']
-df_line2 = df.iloc[1::2, :]
-df_line2.columns = ['Line Number', 'Satellite Catalog Number', 'Inclination', 'Raan', 'Ecc', 'Arg perigee',
-                    'Mean anomaly', 'Mean motion']
+with open(file_sanitised) as f:
+    tle_lines = f.readlines()
 
+tles = {}
+satellites = {}
 ts = load.timescale()
-sat_test = EarthSatellite(line1=df_line1.iloc[0].to_string(), line2=df_line2.iloc[0].to_string())
-satellites = {EarthSatellite(line1=df_line1.iloc[i].to_string(), line2=df_line2.iloc[i].to_string())
-              for i in range(len(df_line1-1))}
+for i in range(0, len(tle_lines)-1, 2):
+    tles['{i}'.format(i=int(i/2))] = [tle_lines[i], tle_lines[i+1]]
+    satellites['{i}'.format(i=int(i/2))] = EarthSatellite(line1=tles['{i}'.format(i=int(i/2))][0],
+                                                          line2=tles['{i}'.format(i=int(i/2))][1])
+satellite = satellites['0']
+t = ts.now()
+geocentric = satellite.at(t)
+print(geocentric.position.km)
+
+bluffton = wgs84.latlon(28.300697, -16.509675, 2390)
+diff = satellite - bluffton
+topocentric = diff.at(t)
+alt, az, distance = topocentric.altaz()
+print(alt, az, distance.m)
+
+# How to get proper times? Need to be in JD
+satellitesgp = Satrec.twoline2rv(tles['0'][0], tles['0'][1])
+jd, fr = 2458827, 0.362605
+e, r, v = satellitesgp.sgp4(jd, fr)
